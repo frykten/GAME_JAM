0263634ec49d76fba1f2574cce13a2127f005f18
frykten <fry.kten.art@gmail.com>
2018-04-11 12:39:59 +0200

    Implementation des mécaniques principales du jeu

--- public/game.js
+++ public/game.js
@@ -1,59 +1,171 @@
-var randCanvas = $("#rand-canvas"),
-	spaceCanvas = $("#space-canvas");
+var randCanvas = $("#rand-game"),
+	spaceCanvas = $("#space-game");
 var stage, w, h, loader;
-var ship;
+var ship, cube, laser, expl;
 
-/*
-var data = {
-        images: ["./assets/2D/test.png"],
-        frames: {width:64, height:64, count:5}
-    };
+var timer;
+var attack, speed, fuel;
 
-var init = () => {
-	stage = new createjs.StageGL("space-canvas");
+var randNum, chosNum;
+
+var isWon = false;
+
+function buttons() {
+	let btns = $("#btns");
 	
-	w = stage.canvas.width = spaceCanvas.width();
-	h = stage.canvas.height = spaceCanvas.height();
+	for (let i = 0; i < 10; i++) {
+		let btn = $("<button>");
+		btn
+			.attr("data-num", i)
+			.addClass("btn numbers")
+			.text(i)
+			.click(chooseNum);
+		
+		btns.append(btn);
+	}
+}
+
+
+function init() {
+	ship = $("#ship");
+	cube = $("#cube");
+	timer = 3;
+	fuel = 15;
+	randNum = parseInt(Math.random() * 10);
 	
-	handleComplete();
+	buttons();
+	
+	
+	console.log();
 };
 
-function handleComplete() {
-	let spriteShip = new createjs.SpriteSheet({
-        images: ["./assets/2D/test.png"],
-        frames: {width:64, height:64, count:5},
-		framerate: 5,
-		"animations": {
-			"normal": [0, 5, "normal", 0.5]
+function clear() {
+	chosNum = null;
+	randNum = parseInt(Math.random() * 10);
+	time();
+	isWon = false;
+	
+	$(".btn").removeClass("true false");
+	
+	speak("Let's do it again...");
+}
+
+function time() {
+	timer = timer -1;
+	
+	if (isWon || !chosNum)
+		timer = 3;
+	if (timer == 0)
+		lose();
+	else if (!isWon)
+		refuel(-1);
+	
+	$("#timer").text("Timer: " + timer);
+}
+
+function refuel(num) {
+	fuel = fuel + num;
+	if (fuel == 0) {
+		lose();
+	}
+	
+	$("#fuel").text("Fuel: " + fuel);
+}
+
+function lose() {
+	speak("Lol! U suck!");
+	refuel(15 - fuel)
+	return window.setTimeout(() => {
+			clear()
+		}, 2000);
+}
+
+function chooseNum() {
+	let tBtn = $(this);
+	chosNum = tBtn.data("num");
+
+	if (isWon) {
+		return clear();
+	}
+	
+	if (chosNum == randNum) {
+		isWon = true;
+		tBtn.addClass("true");
+		speak("You won, jackass...");
+		window.setTimeout(() => {
+			speak("Click a button if you dare!")
+		}, 2000)
+	}
+	else if (chosNum < randNum) {
+		tBtn.addClass("false");
+		speak("Try bigger guns, ya smurf!");
+	}
+	else {
+		tBtn.addClass("false");
+		speak("Nay... Don't think u're better than us.");
+	}
+	
+	shootLaser();
+	
+	if (!isWon)
+		time();
+}
+
+function shootLaser() {
+	laser = $("<img>").attr("src", "assets/2D/laser/laser_01.png").addClass("laser");
+	spaceCanvas.append(laser);
+	
+	laser.position({
+		at: "left",
+		of: ship,
+		using: (e, ui) => {
+			laser.css({
+				top: e.top,
+				left: e.left,
+			});
 		}
-    });
+	});
 	
-	ship = new createjs.Sprite(spriteShip);
-	ship.y = 0;
-	
-	stage.addChild(ship);
-	createjs.Ticker.addEventListener("tick", tick);
+	renderLaser();
 }
 
-function tick(event) {
+function renderLaser() {
+	new Promise( (resolve, rej) => {
+		let pos;
+		if (isWon) 
+			pos = "+=70vh";
+		else
+			pos = "+=90vh";
+		laser.animate({top: pos}, 1000, "linear");
+		
+		resolve("Sent");
+	}).then(() => {
+		window.setTimeout(() => {
+			laser.remove();
+		}, 1000);
+	}); 
 	
-	stage.update(event);
+//	if (isWon)
+		explosion();
 }
-*/
-var init = () => {};
 
-var config = {
-        width: spaceCanvas.width(),
-        height: spaceCanvas.height(),
-        scene: {
-            create: create
-        }
-    };
+function explosion() {
+	let expl = $("#expl");
+	let src = "assets/2D/expl/expl_01.png";
+	expl.css("display", "block");
+	let i = 1;
+	
+	while (i <= 5) {
+		window.setTimeout(() => {
+			src = "assets/2D/expl/expl_0" + i + ".png";
+			expl.attr("src", src);
+			i++;
+		}, 1000);
+	}
+}
 
-var game = new Phaser.Game(config);
-
-function create ()
-    {
-        var logo = this.add.image(400, 100, './assets/2D/test.png');
-console.warn("smurf?");
-    };
+function speak(sentence) {
+	let speaker = $("#speaker");
+	
+	speaker.text(sentence);
+}
